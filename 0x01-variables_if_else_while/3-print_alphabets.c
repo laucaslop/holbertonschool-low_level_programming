20db4585fc41bc51c2144d4d5cd77221c18237e9
laura <1495@holbertonschool.com>
2020-02-06 19:14:52 +0000

    aBcD...

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints whether a random number is postive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+	char C;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		putchar (c);
+	}
+	for (C = 'A' ; C <= 'Z' ; C++)
+	{
+		putchar (C);
+	}
+	putchar('\n');
+
+	return (0);
+}
