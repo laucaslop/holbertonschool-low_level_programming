21f2359c16eca4c95a5d0eeb3c8e1303a8c2cc31
laura <1495@holbertonschool.com>
2020-02-06 21:15:04 +0000

    numbers separated by ,

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: 0
+ */
+int main(void)
+
+{
+	int n;
+
+	while (n <= '9')
+
+{
+	putchar (n);
+	if (n != '9')
+
+	{
+		putchar (' ');
+		putchar (',');
+	}
+	n++;
+}
+	putchar('\n');
+
+	return (0);
+}
