83a14b20cb6ed59000462e622bba96059c6613f7
laura <1495@holbertonschool.com>
2020-02-06 20:18:16 +0000

    alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints whether a random number is postive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'z';
+
+	while
+	(c >= 'a')
+	{
+		putchar (c);
+		c--;
+	}
+	putchar('\n');
+
+	return (0);
+}
