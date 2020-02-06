658048c5ad860dcb6a3a49c27214ccad2d80ac6b
laura <1495@holbertonschool.com>
2020-02-06 15:54:13 +0000

    fix space issues

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,30 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
+/**
+ * main - prints whether a random number is postive or negative
+ * Return: 0
+ */
 int main(void)
-{
-	int n;
+
+{	
+
+int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-        {
-        printf("%i is positive\n", n);
-        } 
-        else if (n = 0)
-        {
-        printf("%i is zero\n", n);
-        }
-        else (n < 0)
 	{
-        printf("%i is negative\n", n);
-        }
-        return (0);
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
 }
