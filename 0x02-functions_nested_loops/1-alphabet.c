ad2d898caa7bef511caca7542585df3fdba8cb5b
laura <1495@holbertonschool.com>
2020-02-10 22:59:48 +0000

    tasks fix issues

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - prints the alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		_putchar (c++);
+	}
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,5 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+void print_alphabet(void);
+
+#endif
