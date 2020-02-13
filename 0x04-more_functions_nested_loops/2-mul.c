bb13abeece9fbdeecd0d2eb008df27829ae1ade1
laura <1495@holbertonschool.com>
2020-02-13 17:48:28 +0000

    complete task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - check the code for Holberton School students.
+ *@a: first multiplication operand
+ *@b: second multiplication operand
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
