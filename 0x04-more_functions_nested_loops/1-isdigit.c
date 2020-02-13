59601b40919e21a2629ebd851c4125a535262e49
laura <1495@holbertonschool.com>
2020-02-13 17:28:06 +0000

    fix gcc issues

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,10 +4,10 @@
  * _isdigit - returns 1 if c is a digit
  * @c: character to check for lowercase
  *
- * Return: returns 0 if not
+ * Return: returns 0 otherwise
  */
 
 int _isdigit(int c)
 {
-	return (c = '0' && c < '9');
+	return (c >= '0' && c <= '9');
 }
