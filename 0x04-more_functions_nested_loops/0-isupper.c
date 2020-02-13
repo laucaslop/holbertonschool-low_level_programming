7362483d8b5dae8a0d03b9e76ecfbca17f615221
laura <1495@holbertonschool.com>
2020-02-13 16:57:52 +0000

    complete functions tasks

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isupper - returns 1 if is c uppercase character
+ * @c: character to check for uppercase
+ *
+ * Return: returns 1 if is a uppercase character, returns 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+
+	return (1);
+
+else
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - returns 1 if c is a digit
+ * @c: character to check for lowercase
+ *
+ * Return: returns 0 if not
+ */
+
+int _isdigit(int c)
+{
+	return (c = '0' && c < '9');
+}
