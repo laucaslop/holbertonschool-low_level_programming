8fe993f8dbb466adfe892f5c292b2121c6ee76fd
laura <1495@holbertonschool.com>
2020-02-14 00:49:03 +0000

    fix gcc issues

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+* print_diagonal - draw a diagonal line
+* return - void
+* @size: integer
+*/
+
+void print_triangle(int size)
+{
+	int height;
+	int width;
+	int draw;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (height = 1; height <= size; height++)
+	{
+		for (width = 1; width <= (size - height); width++)
+		{
+			_putchar(' ');
+		}
+		for (draw = 1; draw <= height; draw++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
