64379a8dccdcc8cbb9c7b3ad88a7800b6bf5160d
laura <1495@holbertonschool.com>
2020-03-20 04:48:46 +0000

    header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,9 @@
-#ifndef LINKLIST
-#define LINKLIST
+#ifndef LIST_H
+#define LIST_H
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 /**
 * struct list_s - main
 * @str: string
@@ -13,8 +17,6 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-#include <stdlib.h>
-int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
