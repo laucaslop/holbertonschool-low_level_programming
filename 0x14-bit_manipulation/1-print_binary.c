3bb379ebb5772ec5ce7439d92b7e113d9fa2ecc1
laura <1495@holbertonschool.com>
2020-03-27 03:54:28 +0000

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+* print_binary - prints the binary representation of a number
+* @n: number to print
+*/
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	_putchar('0');
+
+	else
+	bi_print(n);
+}
+
+/**
+* bi_print - prints the binary representation of a number
+* @n: number to print
+*/
+void bi_print(unsigned long int n)
+{
+	if (n == 0)
+	return;
+
+	bi_print(n >> 1);
+
+	if ((n & 1) == 1)
+	_putchar('1');
+
+	if ((n & 1) == 0)
+	_putchar('0');
+}
+
