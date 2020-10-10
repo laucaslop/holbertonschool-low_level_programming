71eeddd1754339d99b7d86e022a8503ef81d2339
laucaslop <1495@holbertonschool.com>
2020-10-10 05:10:00 +0000

    fix betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,43 +1,46 @@
 #include "hash_tables.h"
+
 /**
 * hash_table_set -  adds an element to the hash table
 * @ht: hash table ptr
 * @key:  key of the array
 * @value: associated value with the key
+* Return: 1 on succes, 0 on fail case
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *entry = NULL;
-	hash_node_t *new = NULL;
-	unsigned long int index = 0;
+	char *var = NULL;
+	unsigned long int idx = 0;
+	hash_node_t *tmp = NULL;
+	hash_node_t *aux_ptr = NULL;
 
-	if (ht == NULL || key == NULL || value == NULL)
+	if (strlen(key) == 0 || key == NULL)
 		return (0);
-
-	index = key_index((unsigned char *)key, ht->size);
-	entry = ht->array[index];
-
-	while (entry != NULL)
+	if (ht == NULL || value == NULL)
+		return (0);
+	var = strdup(value);
+	if (var == NULL)
+		return (0);
+	idx = key_index((unsigned char *)key, ht->size);
+	tmp = ht->array[idx];
+	while (tmp)
 	{
-		if (strcmp(entry->key, key) == 0)
+		if (strcmp(tmp->key, key) == 0)
 		{
-			free(entry->value);
-			entry->value = strdup(value);
-			if (entry->value == NULL)
-				return (0);
+			free(tmp->value);
+			tmp->value = var;
+			tmp->value = strdup(value);
+			free(var);
 			return (1);
 		}
-		entry = entry->next;
+		tmp = tmp->next;
 	}
-	new = malloc(sizeof(hash_node_t));
-
-	if (!new)
+	aux_ptr = malloc(sizeof(hash_node_t));
+	if (aux_ptr == NULL)
 		return (0);
-
-	new->key = strdup(key);
-	new->value = strdup(value);
-	if (new->key == NULL  || new->value == NULL)
-		return (0);
-	new->next = ht->array[index];
-	ht->array[index] = new;
-	return
+	aux_ptr->key = strdup(key);
+	aux_ptr->value = var;
+	aux_ptr->next = ht->array[idx];
+	ht->array[idx] = aux_ptr;
+	return (1);
+}
